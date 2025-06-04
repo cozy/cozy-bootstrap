53f135a0a2158c6cf32b1dbe80907eae20ed6174
Bruno Michel <bruno@cozycloud.cc>
2025-06-04 10:53:56 +0200

    feat: Remove dark mode
    The dark mode was not officially supported, and its support is
    incomplete. We have some bugs with it, so we decided to remove it.
    It may come back later this year when/if we will have time to make it
    work properly.

--- screenshots/makeDarkMode.js
+++
@@ -1,6 +0,0 @@
-// https://playwright.dev/docs/api/class-page#page-emulate-media
-module.exports = async (page, scenario) => {
-  console.log("SWITCHING TO DARK for " + scenario.label)
-
-  await page.emulateMedia({ colorScheme: 'dark' });
-}
